2a7f9d004951161a6ce6f6d92423104f23c76b36
Rags <rags@acm.org>
2017-04-30 03:01:29 -0400

    apt-get update without prompting

--- hdinsight-genomics-adam/adam-installer-v03.sh
+++ hdinsight-genomics-adam/adam-installer-v03.sh
@@ -1,7 +1,7 @@
 # Set Java version to 1.8
 export JAVA_HOME=
 add-apt-repository -y ppa:webupd8team/java
-apt-get update
+apt-get -y update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 apt-get install -y oracle-java8-installer
 apt install oracle-java8-set-default
